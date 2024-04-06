662ce239d2c2801be6bce1482d31568fd1cebdde
Izaac Haro <haro_izaac_benjami01@student.smc.edu>
2024-04-06 00:18:45 -0700

    finished landing page

--- scripts.js
+++ scripts.js
@@ -23,7 +23,7 @@
  * 
  */
 
-
+/*
 const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
 const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
 const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
@@ -91,4 +91,4 @@ function quoteAlert() {
 function removeLastCard() {
     titles.pop(); // Remove last item in titles array
     showCards(); // Call showCards again to refresh
-}
+}*/
